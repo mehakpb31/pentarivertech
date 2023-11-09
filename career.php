ac294b010a67a5fcd17a6539fa3e7981ec002e2e
Mehak <mehakdeep153153@gmail.com>
2023-11-08 18:23:56 -0800

    Initial commit

---
+++ career.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "clients";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO clients (first_name,last_name,email,phone,organization,message) VALUES (?, ?)");
+$stmt->bind_param("ss",$first_name,$last_name,$email,$phone,$organization,$message);
+
+// Set parameters and execute
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$organization = $_POST['organization'];
+$message = $_POST['message'];
+
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
+?>
